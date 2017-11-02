b30a819bb5b99dcf76130dee000bb63b22385dde
da99 <i-hate-spam-1234567@mailinator.com>
2017-11-01 19:08:48 -0500

    Removed: obsolete specs.

--- specs/parser/02.def/specs.cr
+++ specs/parser/02.def/specs.cr
@@ -1,8 +1,26 @@
 
 describe "DA_STYLE::Parser def" do
 
-  it "works" do
+  it "renders the block of css code" do
     should_eq actual!, expected!
   end # === it "works"
+
+  it "uses a new scope" do
+    input = %[
+      RADIUS = 1px;
+      def border-radius(RADIUS) { border-radius: {{RADIUS}}; }
+      div {
+        border-radius(2px);
+        border-radius: {{RADIUS}};
+      }
+    ]
+    expected = %[
+      div {
+        border-radius: 2px;
+        border-radius: 1px;
+      }
+    ]
+    should_eq SPEC_PARSER.new(input, __DIR__).to_css, expected
+  end
 
 end # === desc "DA_STYLE::Parser"
