cb500609d6aca665bd09a9b531273e993ac37a9d
da99 <i-hate-spam-1234567@mailinator.com>
2018-02-09 00:08:49 -0600

    Dev. checkpoint.

--- specs/specs/07.invalid.property_values.cr
+++ specs/specs/07.invalid.property_values.cr
@@ -1,16 +1,15 @@
 
 describe "Parser invalid property values" do
 
-  {% for x in system("find specs/sample-errors/ -maxdepth 1 -type f -iname *.css ").split("\n").map(&.strip).reject(&.empty?).sort %}
-    {% name = x.split("/").last.split(".css").first %}
-    it "raises DA_CSS::CSS_Author_Error for invalid input: {{name.id}}" do
-      input = File.read("{{x.id}}")
+  it "raises DA_CSS::CSS_Author_Error for: 10vpi" do
+    input = %[
+      body { border: 10vpi; }
+    ]
 
-      assert_raises(DA_CSS::CSS_Author_Error) {
-        SPEC_PARSER.to_css(input)
-      }
-    end # === it "raises Node::Property::Invalid_Value for invalid input: "
-  {% end %}
+    assert_raises(DA_CSS::CSS_Author_Error) {
+      SPEC_PARSER.to_css(input)
+    }
+  end # === it "raises Node::Property::Invalid_Value for invalid input: "
 
 
 end # === desc "Parser invalid property values"
